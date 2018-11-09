b9f8abe79b711d01e9737de37a533d0b9b041e05
Kevin Ling <lingkh107@gmail.com>
2018-11-09 22:28:56 +0000

    Added Firebase. You now need .env to run code

--- mobile/App.js
+++ mobile/App.js
@@ -1,13 +1,29 @@
 import {
   createStackNavigator,
 } from 'react-navigation';
+import firebase from "firebase";
 
 import Camera from './pages/Camera';
 import HomeScreen from './pages/Home';
+
+require('dotenv').config()
 
 const Navigation = createStackNavigator({
   Home: { screen: HomeScreen },
   QR: {screen: Camera },
 });
 
+const config = {
+  apiKey: proces.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: procees.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID
+};
+
+const firebaseApp = firebase.initializeApp(config);
+
+const rootRef = firebase.database().ref();
+const itemsRef = rootRef.child('items');
 export default Navigation;
