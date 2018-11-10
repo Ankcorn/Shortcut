8b38143db3082de58665dc0a0deb9749773a5521
Kevin Ling <lingkh107@gmail.com>
2018-11-10 23:02:18 +0000

    Added webpack version of tvm application

---
+++ tvm-webpack/src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+import env from './env'
+const config = {
+  apiKey: env.API_KEY,
+  authDomain: env.AUTH_DOMAIN,
+  databaseURL: env.DATABASE_URL,
+  projectId: env.PROJECT_ID,
+  storageBucket: env.STORAGE_BUCKET,
+  messagingSenderId: env.MESSAGING_SENDER_ID
+};
+const firebaseApp = firebase.initializeApp(config);
+
+export default firebase.messaging();
--- tvm/scripts/tvm.js
+++ tvm/scripts/tvm.js
@@ -1,8 +1,18 @@
+const firebase = require("firebase")
+
 window.onload = function() {
   let qrHelper = new QRCodeHelper();
   qrHelper.displayCode("http://www.google.com");
   tickSecond();
   setInterval(tickSecond, 2000);
+
+  messaging.requestPermission().then(function() {
+    console.log('Notification permission granted.');
+    // TODO(developer): Retrieve an Instance ID token for use with FCM.
+    // ...
+  }).catch(function(err) {
+    console.log('Unable to get permission to notify.', err);
+  });
 };
 
 function QRCodeHelper() {
