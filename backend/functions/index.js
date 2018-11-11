d36f2ef37dcfd5cd9e6ff501d3703e9b95e98658
Kevin Ling <lingkh107@gmail.com>
2018-11-11 02:49:44 +0000

    Hook up messaging

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,21 +1,28 @@
 const functions = require('firebase-functions');
+const request = require('request');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 exports.pairTvm = functions.https.onRequest((req, res) => {
     body = req.body
     console.log('new message in the queue', body)
-    const payload = {notification: {
-            userId: body.userId
-        }
+    var key = 'service key';
+    var to = 'token';
+    var notification = {
+      'userId': body.userId,
     };
-    return admin.messaging().sendToTopic(body.tvmId,payload)
-        .then(function(response){
-                console.log('Notification sent successfully:',response);
-                return true;
-        }) 
-        .catch(function(error){
-                console.log('Notification sent failed:',error);
-                return false;
-    });
+    
+    request.post({
+      'headers': {
+        'Authorization': 'key=' + key,
+        'Content-Type': 'application/json'
+      },
+      'url': 'https://fcm.googleapis.com/fcm/send',
+      'body': JSON.stringify({
+        'notification': notification,
+        'to': to
+      })
+    }, function(error, response, body){
+        console.log(body);
+    })
   });
