5066673699326eedac7eb0a83d737a9919ac07a0
Kevin Ling <contact@kevinling.xyz>
2018-11-12 13:00:13 +0000

    Reintroduce TVM code

---
+++ tvm/firebase-messaging-sw.js
@@ -0,0 +1,19 @@
+importScripts('/__/firebase/5.5.6/firebase-app.js');
+importScripts('/__/firebase/5.5.6/firebase-messaging.js');
+importScripts('/__/firebase/init.js');
+
+var messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  var notificationTitle = 'Background Message Title';
+  var notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+  };
+
+  return self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
+// [END background_handler]
